c1ae9d0306acc3d3a180dcba0d6d1d59c6576532
dt-55 <dtankani@gmail.com>
2024-03-07 08:11:22 -0600

    initial commit

---
+++ src/main/java/com/Dparadise/mygarden/App.java
@@ -0,0 +1,13 @@
+package com.Dparadise.mygarden;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Dparadise/mygarden/AppTest.java
@@ -0,0 +1,20 @@
+package com.Dparadise.mygarden;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
